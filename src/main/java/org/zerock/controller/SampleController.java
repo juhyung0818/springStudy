f4418c08a2364cf5a60b1479cf94a505491363a6
윤주형 <juhyung0818@naver.com>
2016-07-11 14:35:35 +0900

    part3 test

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,5 +1,12 @@
 package org.zerock.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.SampleVO;
@@ -20,5 +27,49 @@ public class SampleController {
 		vo.setLastName("yun");
 		vo.setMno(123);
 		return vo;
+	}
+	
+	@RequestMapping("/sendList")
+	public List<SampleVO> sendList(){
+		List<SampleVO> list = new ArrayList<>();
+		for(int i=0; i<10; ++i){
+			SampleVO vo = new SampleVO();
+			vo.setFirstName("juhyung");
+			vo.setLastName("yun");
+			vo.setMno(i);
+			list.add(vo);
+		}
+		return list;
+	}
+	
+	@RequestMapping("/sendMap")
+	public Map<Integer, SampleVO> sendMap(){
+		Map<Integer, SampleVO> map = new HashMap<>();
+		for(int i=0; i<10; ++i){
+			SampleVO vo = new SampleVO();
+			vo.setFirstName("jhyung");
+			vo.setLastName("yun");
+			vo.setMno(i);
+			map.put(i, vo);
+		}
+		return map;
+	}
+	
+	@RequestMapping("/sendErrorAuth")
+	public ResponseEntity<Void> sendListAuth(){
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
+	
+	@RequestMapping("/sendErrorNot")
+	public ResponseEntity<List<SampleVO>> sendListNot(){
+		List<SampleVO> list = new ArrayList<>();
+		for(int i=0; i<10; ++i){
+			SampleVO vo = new SampleVO();
+			vo.setFirstName("tuhyung");
+			vo.setLastName("yun");
+			vo.setMno(i);
+			list.add(vo);
+		}
+		return new ResponseEntity<>(list, HttpStatus.NOT_FOUND);
 	}
 }
